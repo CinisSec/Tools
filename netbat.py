bb00268de92ab730d10d4d265a97d3498627731b
CinisSec <gregorypro@protonmail.com>
2017-06-07 23:38:01 +0200

    should be working now....

--- netbat.py
+++ netbat.py
@@ -16,139 +16,6 @@ target              = ""
 upload_destination  = ""
 port                = 0
 
-def usage():
-    print "Netbat Net Tool"
-    print
-    print "Usage: netbat.py -t target_host -p port"
-    print "-l --listen              - listen on [host]:[port] for"
-    print "-e --execute=file        - execute file upon receiving a connection"
-    print "-c --command             - initialize a command shell"
-    print "-u --upload=destination  - upon receiving connection upload a file \
-                                      and write to [destination]"
-    print
-    print
-    print "Examples: "
-    print "netbat.py -t 192.168.0.1 -p 5555 -l -c"
-    print "netbat.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
-    print "netbat.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
-    print "echo 'ABCDEFGHI' | ./netbat.py -t 192.168.11.12 -p 135"
-    sys.exit(0)
-
-def main():
-    global listen
-    global port
-    global command
-    global execute
-    global upload_destination
-    global target
-
-    if not len(sys.argv[1:]):
-        usage()
-
-    # read CLI options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu",["help","listen","execute","target","port","command","upload"])
-    except getopt.GetoptError as err:
-        print str(err)
-        usage()
-
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            usage()
-        elif o in ("-l", "--listen"):
-            listen = True
-        elif o in ("-e", "--execute"):
-            execute = a
-        elif o in ("-c", "--commandshell"):
-            command = True
-        elif o in ("-u", "--upload"):
-            upload_destination = a
-        elif o in ("-t", "--target"):
-            target = a
-        elif o in ("-p", "--port"):
-            port = int(a)
-        else:
-            assert False, "Unhandled Option"
-    
-    # listen or just send data from stdin?
-    if not listen and len(target) and port > 0:
-
-        # read in buffer from CLI
-        # this will block, so send CTRL-D if not sending input
-        # to CLI
-        buffer = sys.stdin.read()
-
-        # send data off
-        client_sender(buffer)
-
-    # listen and potentially upload, execute commands, drop shell back
-    # depending on the CLI options
-    if listen:
-        server_loop()
-
-def client_sender(buffer):
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        # connect to target
-        client.connect((target, port))
-
-        if len(buffer):
-            
-            client.send(buffer)
-
-        while True:
-
-            #wait for data response
-            recv_len = 1
-            response = ""
-
-            while recv_len:
-
-                data        = client.recv(4096)
-                recv_len    = len(data)
-                response   += data
-
-                if recv_len < 4096:
-                    break
-
-            print response
-
-            # wait for more input
-            buffer  = raw_input("")
-            buffer += "\n"
-
-            # send buffer
-            client.send(buffer)
-    except:
-
-        print "[*] Exception! Exiting."
-
-        # close connection
-        client.close()
-
-def server_loop():
-    global target
-    global port
-
-    # if no target is definded, we listen on all interfaces
-    if not len(target):
-        target = "0.0.0.0"
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((target, port))
-
-    server.listen(5)
-
-    while True:
-        client_socket, addr = server.accept()
-
-        # spin off a thread to handle new client
-        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
-        client_thread.start()
-
-
 def run_command(command):
     # trim new line
     command = command.rstrip()
@@ -220,5 +87,139 @@ def client_handler(client_socket):
             client_socket.send(response)
 
 
+def server_loop():
+    global target
+    global port
+
+    # if no target is definded, we listen on all interfaces
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        # spin off a thread to handle new client
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+
+def client_sender(buffer):
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # connect to target
+        client.connect((target, port))
+
+        if len(buffer):
+            
+            client.send(buffer)
+
+        while True:
+
+            #wait for data response
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+
+                data        = client.recv(4096)
+                recv_len    = len(data)
+                response   += data
+
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            # wait for more input
+            buffer  = raw_input("")
+            buffer += "\n"
+
+            # send buffer
+            client.send(buffer)         
+    except:
+
+        print "[*] Exception! Exiting."
+
+        # close connection
+        client.close()
+
+
+def usage():
+    print "Netbat Net Tool"
+    print
+    print "Usage: netbat.py -t target_host -p port"
+    print "-l --listen              - listen on [host]:[port] for"
+    print "-e --execute=file        - execute file upon receiving a connection"
+    print "-c --command             - initialize a command shell"
+    print "-u --upload=destination  - upon receiving connection upload a file \
+                                      and write to [destination]"
+    print
+    print
+    print "Examples: "
+    print "netbat.py -t 192.168.0.1 -p 5555 -l -c"
+    print "netbat.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
+    print "netbat.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
+    print "echo 'ABCDEFGHI' | ./netbat.py -t 192.168.11.12 -p 135"
+    sys.exit(0)
+
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    # read CLI options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",["help","listen","execute","target","port","command","upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+    
+    # listen or just send data from stdin?
+    if not listen and len(target) and port > 0:
+
+        # read in buffer from CLI
+        # this will block, so send CTRL-D if not sending input
+        # to CLI
+        buffer = sys.stdin.read()
+
+        # send data off
+        client_sender(buffer)
+
+    # listen and potentially upload, execute commands, drop shell back
+    # depending on the CLI options
+    if listen:
+        server_loop()
 
 main()
